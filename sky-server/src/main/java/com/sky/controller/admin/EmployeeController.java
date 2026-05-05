15eff53c09c89369e48ae9da8a4f8f2133178510
yzd <yzd01234567890@163.com>
2026-05-05 16:22:10 +0800

    编辑员工信息业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -105,7 +105,8 @@ public class EmployeeController {
     /**
      * 启用禁用员工账号
      * @return
-     * @param status,id
+     * @param status
+     * @param  id
      */
     @PostMapping("/status/{status}")
     @ApiOperation("启用禁用员工账号")
@@ -114,4 +115,30 @@ public class EmployeeController {
         employeeService.startOrStop(status,id);
         return Result.success();
     }
+
+    /**
+     * 查询员工信息
+     * @param id
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("查询员工信息")
+    public Result<Employee> getById(@PathVariable("id") Long id){
+        Employee employee =employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     * @return
+     */
+
+    @PutMapping()
+    @ApiOperation("编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息：{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -37,4 +37,8 @@ public interface EmployeeMapper {
 
 
     void update(Employee employee);
+
+
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
