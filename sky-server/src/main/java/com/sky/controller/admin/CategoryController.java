c389fd21b4a0f811936aef2aa9d6bd3751d86021
yzd <yzd01234567890@163.com>
2026-05-05 20:01:03 +0800

    分类模块业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,106 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.exception.DeletionNotAllowedException;
+import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ *分类管理
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Api(tags = "分类管理")
+@Slf4j
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 分类分页查询
+     * @param
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分类分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+       PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增分类")
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        log.info("新增分类：{}",categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    @DeleteMapping
+    @ApiOperation("根据id删除分类")
+    public Result deleteById(Long id){
+        log.info("根据id删除分类：{}",id);
+        categoryService.deleteById(id);
+
+        return Result.success();
+    }
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改分类")
+    public Result update(@RequestBody CategoryDTO categoryDTO){
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用、禁用分类")
+    public Result startOrStop(@PathVariable("status") Integer status,Long id){
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据类型查询分类")
+    public Result<List<Category>>  list(Integer type){
+        List<Category>  list =categoryService.list(type);
+        return Result.success(list);
+    }
+
+}
