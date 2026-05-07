d98f74fc4efe7680df3d827d874340ece376fd95
yzd <yzd01234567890@163.com>
2026-05-07 21:13:23 +0800

    菜品分页查询功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 菜品管理
@@ -38,5 +37,15 @@ public class DishController {
        return Result.success();
     }
 
+    /**
+     * 菜品管理分页查询
+     */
+    @ApiOperation("菜品管理分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品管理分页查询，参数：{}",dishPageQueryDTO);
+        PageResult pageResult = dishService. pageQuery(dishPageQueryDTO);
 
+        return Result.success(pageResult);
+    }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,7 +3,9 @@ package com.sky.service;
 
 import com.sky.annotation.AutoFill;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
 
 /**
  * 菜品管理
@@ -15,5 +17,11 @@ public interface DishService {
      */
     public void saveWithFlavor(DishDTO dishDTO);
 
-
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
