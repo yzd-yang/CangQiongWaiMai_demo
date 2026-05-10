69cff5b249a3e17b6e610100a9a563a533668c0e
yzd <yzd01234567890@163.com>
2026-05-10 22:40:51 +0800

    商品浏览业务功能

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -19,5 +19,4 @@ public class WeChatProperties {
     private String weChatPayCertFilePath; //平台证书
     private String notifyUrl; //支付成功的回调地址
     private String refundNotifyUrl; //退款成功的回调地址
-
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -6,6 +6,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -31,11 +32,6 @@ public interface SetmealMapper {
     void insert(Setmeal setmeal);
 
     /**
-     * 批量保存套餐和菜品的关联关系
-     * @param setmealDishes
-     */
-    void insertBatch(List<SetmealDish> setmealDishes);
-    /**
      * 分页查询
      * @param setmealPageQueryDTO
      * @return
@@ -57,23 +53,29 @@ public interface SetmealMapper {
      */
     @Delete("delete from setmeal where id = #{setmealId}")
     void deleteById(Long setmealId);
-    /**
-     * 根据套餐id删除套餐和菜品的关联关系
-     * @param setmealId
-     */
-    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
-    void deleteBySetmealId(Long setmealId);
-    /**
-     * 根据套餐id查询套餐和菜品的关联关系
-     * @param setmealId
-     * @return
-     */
-    @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
-    List<SetmealDish> getBySetmealId(Long setmealId);
+
+
 
     /**
      * 修改套餐
      * @param setmeal
      */
     void update(Setmeal setmeal);
+
+    /**
+     * 动态条件查询套餐
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据套餐id查询菜品选项
+     * @param setmealId
+     * @return
+     */
+    @Select("select sd.name, sd.copies, d.image, d.description " +
+            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
+            "where sd.setmeal_id = #{setmealId}")
+    List<DishItemVO> getDishItemBySetmealId(Long setmealId);
 }
