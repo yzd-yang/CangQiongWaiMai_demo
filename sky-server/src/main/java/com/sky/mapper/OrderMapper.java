a9fd581c7084e2945a57ba17ccda14b02705d544
yzd <yzd01234567890@163.com>
2026-05-16 15:54:09 +0800

    订单定时单业务功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -48,5 +49,12 @@ public interface OrderMapper {
      */
     @Select("select count(id) from orders where status=#{status}")
     Integer countStatus(Integer status);
+
+    /**
+     *  查询超时订单
+     * @return
+     */
+    @Select("select * from orders where status=#{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status,LocalDateTime orderTime);
 }
 
