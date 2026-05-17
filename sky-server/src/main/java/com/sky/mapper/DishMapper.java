b66e7928b4485bf9f4a1799b6742e1b8a8332962
yzd <yzd01234567890@163.com>
2026-05-17 11:02:49 +0800

    工作台业务功能开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -72,5 +73,11 @@ public interface DishMapper {
      */
     @Select("SELECT d.* FROM dish as d JOIN  setmeal_dish as s on d.id=s.dish_id where s.setmeal_id=#{id}")
     List<Dish> getBySetmealId(Long id);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
 
