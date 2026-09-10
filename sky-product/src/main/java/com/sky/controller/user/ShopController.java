1636c7c7902867ed97be4668b2bf06544051c740
yzd <yzd01234567890@163.com>
2026-09-10 18:54:43 +0800

    feat: 添加多个模块的 pom.xml 文件及相关功能实现
    新建 sky-file、sky-gateway、sky-pay、sky-product 模块，配置 Maven 依赖，添加自动填充注解及切面，配置 Redis 和 Web MVC，创建分类、菜品、套餐等管理控制器，完善 JWT 令牌校验拦截器，支持 C 端和管理端接口。

---
+++ sky-product/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,37 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Tag(name = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @Operation(summary = "获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = Integer.valueOf(Objects.requireNonNull(redisTemplate.opsForValue().get("SHOP_STATUS")));
+        log.info("获取店铺营业状态为:{}",status==1?"营业中":"打烊中");
+
+        return Result.success(status);
+    }
+}
---
+++ sky-product/src/main/java/com/sky/service/Impl/DishServiceImpl.java
@@ -0,0 +1,223 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.result.PageResult;
+import com.sky.service.DishService;
+import com.sky.utils.FileUploadUtil;
+import com.sky.vo.DishVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+/**
+ * 菜品管理业务实现类
+ */
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+
+    @Autowired
+    private FileUploadUtil fileUploadUtil;
+    /**
+     * 新增菜品和对应的口味
+     *
+     * @param dishDTO
+     */
+    @Override
+    @Transactional//添加事务
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //项菜品表插入1条数据
+        dishMapper.insert(dish);
+        //获取刚插入的菜品的id(主键回显)
+        dish.getId();
+        //接受前端传过来的口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors !=null && !flavors.isEmpty()){
+            for (DishFlavor flavor : flavors) {
+                flavor.setDishId(dish.getId());
+            }
+            //批量插入
+            dishFlavorMapper.insertBatch(flavors);
+
+        }
+        //项口味表插入n条数据
+
+    }
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(),dishPageQueryDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+    /**
+     * 菜品批量删除
+     *
+     * @param ids
+     */
+    @Override
+    @Transactional//添加事务
+    public void deleteBatch(List<Long> ids) {
+        //判断当前菜品是否能够删除---是否存在起售中的菜品？？
+        for (Long id : ids) {
+            Dish dish = dishMapper.getById(id);
+            if(dish.getStatus()== StatusConstant.ENABLE ){
+                //当前菜品处于起售中
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+        //看菜品是否关联套餐
+        List<Long> seleteIds = setmealDishMapper.getSetmealIdsByDishIds(ids);
+        if(seleteIds != null && !seleteIds.isEmpty()){
+            //存在关联套餐
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+
+        //删除菜品表中的菜品数据
+        for (Long id : ids) {
+            //删除菜品的图片
+            Dish dish = dishMapper.getById(id);
+            fileUploadUtil.delete(dish.getImage());
+            log.info("删除图片：{}",dish.getImage());
+
+//            dishMapper.deleteById(id);
+//            dishFlavorMapper.deleteByDishId(id);
+        }
+
+        //批量删除
+            dishMapper.deleteByIds(ids);
+            dishFlavorMapper.deleteByDishIds(ids);
+    }
+
+    @Override
+    public DishVO getByIdWithFlavor(Long dishId) {
+        //根据id查询菜品数据
+        Dish dish = dishMapper.getById(dishId);
+        //根据disid查询口味数据
+        List<DishFlavor> dishFlavors= dishFlavorMapper.getByDishId(dishId);
+        //将查询到的数据封装到VO
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish,dishVO);
+        dishVO.setFlavors(dishFlavors);
+        return dishVO;
+    }
+    /**
+     * 根据id修改菜品基本信息和对应的口味信息
+     *
+     * @param dishDTO
+     */
+    @Override
+    @Transactional
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+
+//TODO
+        //删除菜品的图片
+        Dish byId = dishMapper.getById(dish.getId());
+        if(byId.getImage().equals(dish.getImage())){
+            fileUploadUtil.delete(dish.getImage());
+            log.info("删除图片：{}",dish.getImage());
+
+        }
+        //修改菜品表
+        dishMapper.update(dish);
+
+        //删除原有的口味数据
+        dishFlavorMapper.deleteByDishId(dishDTO.getId());
+        //添加新的口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && !flavors.isEmpty()){
+            for (DishFlavor flavor : flavors) {
+                flavor.setDishId(dishDTO.getId());
+            }
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public List<Dish> list(Long categoryId) {
+        Dish dish = Dish.builder()
+                .status(StatusConstant.ENABLE)
+                .categoryId(categoryId)
+                .build();
+        return dishMapper.list(dish);
+    }
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    public List<DishVO> listWithFlavor(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
+
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //根据菜品id查询对应的口味
+            List<DishFlavor> flavors = dishFlavorMapper.getByDishId(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+
+        return dishVOList;
+    }
+
+    /**
+     * 启售停售
+     * @param status
+     * @param id
+     */
+    @Override
+    @Transactional
+    public void startOrStop(Integer status, Long id) {
+        Dish dish = Dish.builder()
+                .status(status)
+                .id(id)
+                .build();
+        dishMapper.update(dish);
+    }
+
+}
